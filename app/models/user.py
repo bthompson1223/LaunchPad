cdeaa95a962cef4c9b207c12e274d34d2083ded8
Lei Li <lei.xw.li@hotmail.com>
2024-02-16 14:12:16 -0800

    class project done

--- app/models/user.py
+++ app/models/user.py
@@ -17,6 +17,8 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
+    projects = db.relationship('Project', back_populates = 'owner')
+
     @property
     def password(self):
         return self.hashed_password
