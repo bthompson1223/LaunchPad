8b907bacc2b59bb547fc6426e74615beee7f10c8
Chase <chase.agee@gmail.com>
2024-02-16 16:23:57 -0800

    created and finished categories list, created users list (still needs images), created projects (still needs images, categories)

---
+++ app/seeds/user_seeds.py
@@ -0,0 +1,50 @@
+users = [
+    {
+        "id": 1,
+        "username":'Demo',
+        "first_name": "Demo",
+        "last_name": "Lition",
+        "email":'demo@aa.io', 
+        "password":'password'
+     },
+    {
+        "id": 2,
+        "username":'olirodri',
+        "first_name": "Olivia",
+        "last_name": "Rodriguez",
+        "email":'olivia.rodriquez@aa.io', 
+        "password":'password'
+     },
+    {
+        "id": 3,
+        "username":'emilyturner',
+        "first_name": "Emily",
+        "last_name": "Turner",
+        "email":'emily.turner@aa.io', 
+        "password":'password'
+     },
+    {
+        "id": 4,
+        "username":'rajpat',
+        "first_name": "Rajesh",
+        "last_name": "Patel",
+        "email":'rajesh.patel@aa.io', 
+        "password":'password'
+     },
+    {
+        "id": 5,
+        "username":'elenarami',
+        "first_name": "Elena",
+        "last_name": "Ramirez",
+        "email":'elena.ramirez@aa.io', 
+        "password":'password'
+     },
+     {
+        "id": 6,
+        "username":'jessicamartiz',
+        "first_name": "Jessica",
+        "last_name": "Martinez",
+        "email":'jessica.martinez@aa.io', 
+        "password":'password'
+     }
+]
