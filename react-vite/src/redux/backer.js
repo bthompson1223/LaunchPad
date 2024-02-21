2498e7426129733ac04499c93f666a6c22c67eb3
Chase <chase.agee@gmail.com>
2024-02-21 10:19:42 -0800

    testing backed-projects api endpoint

--- react-vite/src/redux/backer.js
+++ react-vite/src/redux/backer.js
@@ -1,21 +1,37 @@
 // action type
 const ADD_BACKER = "backers/ADD_BACKER"
+const GET_BACKINGS = "backers/GET_BACKINGS"
 
 // action creator
-const addBacker = (backer) => {
+const addBacker = (backer) => ({
     type: ADD_BACKER,
     backer
-}
+})
+
+const getBackings = (backings) => ({
+  type: GET_BACKINGS,
+  backings
+}) 
 
 // thunk - add backer to a reward
 export const thunkAddBacker = (rewardId) => async (dispatch) => {
-    const res = await fetch(`/api/rewards/${rewardId}`)
+    const res = await fetch(`/api/rewards/${rewardId}`, {
+      method: "POST"
+    })
 
     if (res.ok) {
-        const rewards = await res.json();
-        dispatch(getRewards(rewards));
+        const backer = await res.json();
+        dispatch(addBacker(backer));
       } else {
         const errs = await res.json();
         return errs;
       }
 } 
+
+export const thunkGetBackings = () => async (dispatch) => {
+  const res = await fetch(`/api/rewards/`)
+}
+
+const initialState = {}
+
+export const 
