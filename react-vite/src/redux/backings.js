f0c19ccf1571a85564a3e05e6ad3fbb1670b307a
Chase <chase.agee@gmail.com>
2024-02-21 15:12:45 -0800

    finished backing deleting

--- react-vite/src/redux/backings.js
+++ react-vite/src/redux/backings.js
@@ -2,7 +2,7 @@
 const ADD_BACKING = "backings/ADD_BACKING"
 const GET_BACKINGS = "backings/GET_BACKINGS"
 const CLEAR_BACKINGS = 'backings/CLEAR_BACKINGS'
-const REMOVE_BACKING = 'backings/REMOVE_BACKING'
+const DELETE_BACKING = 'backings/DELETE_BACKING'
 
 // action creator
 const addBacking = (backing) => ({
@@ -19,27 +19,26 @@ export const clearBackings = () => ({
   type: CLEAR_BACKINGS
 })
 
-const removeBacking = (backingId) => ({
-  type: REMOVE_BACKING,
+const deleteBacking = (backingId) => ({
+  type: DELETE_BACKING,
   backingId
 })
 
 // thunk - add backer to a reward
 export const thunkAddBacking = (rewardId) => async (dispatch) => {
-    const res = await fetch(`/api/rewards/${rewardId}/backing`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: null
-    })
+  const res = await fetch(`/api/rewards/${rewardId}/backing`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: null
+  })
 
-    console.log(res)
-    if (res.ok) {
-        const backing = await res.json();
-        dispatch(addBacking(backing));
-      } else {
-        const errs = await res.json();
-        return errs;
-      }
+  if (res.ok) {
+      const backing = await res.json();
+      dispatch(addBacking(backing));
+    } else {
+      const errs = await res.json();
+      return errs;
+    }
 } 
 
 export const thunkGetBackings = () => async (dispatch) => {
@@ -55,9 +54,20 @@ export const thunkGetBackings = () => async (dispatch) => {
   }
 }
 
-// export const thunkRemoveBacking = (backingId) => async (dispatch) => {
-//   const res = await fetch(`/api/rewards/${rewardId}/`)
-// }
+export const thunkDeleteBacking = (backingId, rewardId) => async (dispatch) => {
+  const res = await fetch(`/api/rewards/${rewardId}/backing/${backingId}`, {
+    method: "DELETE"
+  })
+
+  if (res.ok) {
+    const message = await res.json();
+    dispatch(deleteBacking(backingId))
+    return message
+  } else {
+    const errs = await res.json()
+    return errs
+  }
+}
 
 const initialState = {}
 
@@ -73,6 +83,11 @@ const backingsReducer = (state = initialState, action) => {
       action.backings.forEach(backing => {
         newState[backing.id] = backing
       })
+      return newState
+    }
+    case DELETE_BACKING: {
+      const newState = { ...state }
+      delete newState[action.backingId]
       return newState
     }
     case CLEAR_BACKINGS: {
