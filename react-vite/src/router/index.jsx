a07724e1f03adf07b66f32c9506bcb12826c6019
Lei Li <lei.xw.li@hotmail.com>
2024-02-21 07:44:34 -0800

    fix merge conflict

--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { createBrowserRouter, Outlet } from 'react-router-dom';
 import LoginFormPage from '../components/LoginFormPage';
 import SignupFormPage from '../components/SignupFormPage';
@@ -10,26 +8,8 @@ import RewardList from '../components/Rewards/RewardList/rewardList';
 import CreateProject from '../components/Projects/CreateProject/CreateProject';
 import EditProject from '../components/Projects/EditProject/EditProject';
 import OwnedProjects from '../components/Projects/OwnedProjects/OwnedProjects';
-import CreateReward from '../components/Rewards/CreateReward/CreateReward';
-=======
-=======
->>>>>>> f7de57e8c4703a558382ef354adbd0f6c33c520d
-import { createBrowserRouter, Outlet } from "react-router-dom";
-import LoginFormPage from "../components/LoginFormPage";
-import SignupFormPage from "../components/SignupFormPage";
-import Layout from "./Layout";
-import Categories from "../components/Categories/categories";
-import ProjectDetail from "../components/Projects/ProjectDetail/projectDetail";
-import RewardList from "../components/Rewards/RewardList/rewardList";
-import CreateProject from "../components/Projects/CreateProject/CreateProject";
-import EditProject from "../components/Projects/EditProject/EditProject";
-import OwnedProjects from "../components/Projects/OwnedProjects/OwnedProjects";
 import SplashPage from "../components/SplashPage/splashPage";
-<<<<<<< HEAD
->>>>>>> dev
-=======
 import CreateReward from '../components/Rewards/CreateReward/CreateReward';
->>>>>>> f7de57e8c4703a558382ef354adbd0f6c33c520d
 
 export const router = createBrowserRouter([
   {
@@ -60,10 +40,6 @@ export const router = createBrowserRouter([
       {
         path: "projects",
         element: <Outlet />,
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f7de57e8c4703a558382ef354adbd0f6c33c520d
         children:[
             {
                 path: ":projectId",
@@ -104,40 +80,6 @@ export const router = createBrowserRouter([
             }
         ]
       }
-<<<<<<< HEAD
-=======
-        children: [
-          {
-            path: ":projectId",
-            element: <Outlet />,
-            children: [
-              {
-                index: true,
-                element: <ProjectDetail />,
-              },
-              {
-                path: "rewards",
-                element: <RewardList />,
-              },
-              {
-                path: "edit",
-                element: <EditProject />,
-              },
-            ],
-          },
-          {
-            path: "new",
-            element: <CreateProject />,
-          },
-          {
-            path: "created-projects",
-            element: <OwnedProjects />,
-          },
-        ],
-      },
->>>>>>> dev
-=======
->>>>>>> f7de57e8c4703a558382ef354adbd0f6c33c520d
     ],
   },
 ]);
