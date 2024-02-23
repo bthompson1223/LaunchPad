9d1b053be0b7ca021ed245d4a31d3e62f91f9784
Bryan Thompson <bthompson1223@outlook.com>
2024-02-23 10:34:40 -0600

    category page nav styled to match splash page nav

--- react-vite/src/components/Categories/categories.jsx
+++ react-vite/src/components/Categories/categories.jsx
@@ -5,19 +5,21 @@ import "./categories.css";
 const Categories = () => {
   return (
     <div className="categories-container">
-      <nav className="category-nav">
-        <NavLink to="/categories/all">All</NavLink>
-        <NavLink to="/categories/arts">Arts</NavLink>
-        <NavLink to="/categories/comics-illustration">
-          Comics & Illustration
-        </NavLink>
-        <NavLink to="/categories/design-tech">Design & Tech</NavLink>
-        <NavLink to="/categories/film">Film</NavLink>
-        <NavLink to="/categories/food-craft">Food & Craft</NavLink>
-        <NavLink to="/categories/games">Games</NavLink>
-        <NavLink to="/categories/music">Music</NavLink>
-        <NavLink to="/categories/publishing">Publishing</NavLink>
-      </nav>
+      <div className="category-nav-container">
+        <nav className="category-nav">
+          <NavLink to="/categories/all">All</NavLink>
+          <NavLink to="/categories/arts">Arts</NavLink>
+          <NavLink to="/categories/comics-illustration">
+            Comics & Illustration
+          </NavLink>
+          <NavLink to="/categories/design-tech">Design & Tech</NavLink>
+          <NavLink to="/categories/film">Film</NavLink>
+          <NavLink to="/categories/food-craft">Food & Craft</NavLink>
+          <NavLink to="/categories/games">Games</NavLink>
+          <NavLink to="/categories/music">Music</NavLink>
+          <NavLink to="/categories/publishing">Publishing</NavLink>
+        </nav>
+      </div>
 
       <div>
         <ProjectList />
